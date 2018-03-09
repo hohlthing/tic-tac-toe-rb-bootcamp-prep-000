8417ae95a9f33a7fb7349b4464b48c029b3157e5
michael hohl <mhohl@rocketmail.com>
2018-03-09 19:13:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,6 +35,16 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space != " "
+      count += 1
+    end
+  end
+  count
+end
+
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
