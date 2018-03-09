a8261d70716e63bf38534be4cbe8c60aa6b01dd5
michael hohl <mhohl@rocketmail.com>
2018-03-09 19:14:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,16 @@ def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
-
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if winner(board)
+    puts "Congratulations #{winner(board)}!"
+  else
+    puts "Cat's Game!"
+  end
+end
 
 WIN_COMBINATIONS = [
  [0,1,2], # Top row
